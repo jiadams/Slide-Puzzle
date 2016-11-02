0c51b7509039e0f3accdaf28c2a2c712a14cffd8
Josh Adams <joshua.ian.adams@gmail.com>
2016-11-02 11:13:42 -0400

    Backgrounds and Solvability
    Fixed Solvability Check and Add background image functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     var puzzle = new SlidePuzzle(4,4);
-    $(document).on("click", "button#shuffle-btn", function () {
+    $(document).on("click touchstart", "button#shuffle-btn", function () {
         puzzle.shuffleBoard();
     });
-    $(document).on("click", ".block", function () {
+    $(document).on("click touchstart", ".block", function () {
         puzzle.moveTile(this);
     });
 });
@@ -13,11 +13,13 @@ var SlidePuzzle = function (width,height) {
     this.w = width;
     this.h = height;
     this.arrLen = this.w * this.h;
+    this.gameBackground = './images/planet-bg.jpg';
     this.moves = 0;
     this.solveState = this.buildBoard();
-    this.board = this.solveState;
+    this.board = this.buildBoard();
     this.drawBoard();
     this.boardLine;
+    this.gameStart = false;
 };
 
 SlidePuzzle.prototype.buildBoard = function() {
@@ -47,11 +49,12 @@ SlidePuzzle.prototype.buildBoard = function() {
 };
 
 SlidePuzzle.prototype.shuffleBoard = function() {
-    tempArray = this.boardLine;
+    var tempArray = this.boardLine;
     do {
         tempArray = this.shuffle(tempArray);
     } while (!this.solvable(tempArray));
-    boardArray = this.createMDArray();
+
+    var boardArray = this.createMDArray();
     k = 0;
     for (i = 0; i < this.h; i++) {
         for (j = 0; j < this.w; j++) {
@@ -59,33 +62,36 @@ SlidePuzzle.prototype.shuffleBoard = function() {
             k++;
         }
     }
+    console.log(this.solvable(tempArray));
     this.boardLine = tempArray;
     this.board = boardArray;
+    this.gameStart = true;
+    this.moves = 0;
     this.drawBoard();
 };
 
 SlidePuzzle.prototype.solvable = function(boardArray) {
     var i, j, blank, inv = 0, arrLen = boardArray.length ;
-        for(i = 0; i <arrLen; i++) {
-            if (boardArray[i] < arrLen) {
-                for (j = i +1; j < arrLen; j++) {
-                    if(boardArray[i] > boardArray[j]){
-                        inv ++;
-                    }
+    for(i = 0; i <arrLen; i++) {
+        if (boardArray[i] < arrLen) {
+            for (j = i +1; j < arrLen; j++) {
+                if(boardArray[i] > boardArray[j]){
+                    inv ++;
                 }
-            } else {
-                blank = i + 1;
             }
+        } else {
+            blank = i + 1;
         }
+    }
     if (this.w % 2) {
         if (inv%2) {
             return false;
         }
     } else {
         var blankRow = -((Math.ceil(blank / this.h )) - (this.h +1));
-        if (!inv%2 && blankRow%2) {
+        if (inv%2 && blankRow%2) {
             return false;
-        } else if(!blankRow%2) {
+        } else if(inv%2 === 0 && blankRow%2 === 0) {
             return false;
         }
     }
@@ -119,20 +125,22 @@ SlidePuzzle.prototype.createMDArray = function() {
 };
 
 SlidePuzzle.prototype.drawBoard = function() {
-    var $gameNode = $("#game-board");
+    var $tileNode, $gameNode = $("#game-board");
     $gameNode.empty();
     var i, j;
     for(i = 0; i < this.board.length; i++) {
         for( j = 0; j < this.board[i].length; j++) {
             nodeValue = this.board[i][j];
             if (nodeValue < this.arrLen) {
-                $("<div></div>").addClass("block").html(nodeValue).css({
+                $tileNode = $("<div></div>").addClass("block").html(nodeValue).css({
                     top : 100 * i,
-                    left : 100 * j
+                    left : 100 * j,
+                    
                 }).attr({
                     'data-col' : i,
                     'data-row' : j
-                }).appendTo($gameNode);
+                })
+                $tileNode.appendTo($gameNode);
             } else {
                 $("<div></div>").addClass("block blank").css({
                     top : 100 * i,
@@ -149,7 +157,30 @@ SlidePuzzle.prototype.drawBoard = function() {
         width : (this.w * 100),
         height : (this.h * 100)
     });
+    this.backgroundSetter()
 };
+
+SlidePuzzle.prototype.backgroundSetter = function() {
+    var $tileNode, tileNum, i, j;
+    var w = this.w, h = this.h;
+    var solveState = this.solveState;
+    var bg = this.gameBackground
+    $('div.block').each(function(){
+        $tileNode = $(this);
+        tileNum = Number($tileNode.html());
+        for(i = 0; i < solveState.length; i++) {
+            for(j = 0; j < solveState[i].length; j++) {
+                if (tileNum === solveState[i][j]) {
+                    $tileNode.css({
+                        "background-image" : "url(" + bg + ")",
+                        "background-size"  : w*100 + "px " + h*100 + "px",
+                        "background-position" :  -100*j + "px " + -100*i + "px"
+                    })
+                }
+            }
+        }
+    })
+}
 
 SlidePuzzle.prototype.checkSolve = function () {
     var i, j;
@@ -187,7 +218,10 @@ SlidePuzzle.prototype.moveTile = function (element) {
                 'col' : col,
                 'row' : row
             });
-        this.moves ++;
+        this.moveCounter();
+        if(this.checkSolve()) {
+            console.log("win");
+        }
     } else if(row > 0 && this.board[col][row-1] === this.arrLen) {
         temp = this.board[col][row-1];
         this.board[col][row-1] = this.board[col][row];
@@ -207,7 +241,10 @@ SlidePuzzle.prototype.moveTile = function (element) {
                 'col' : col,
                 'row' : row
             });
-        this.moves ++;
+        this.moveCounter();
+        if(this.checkSolve()) {
+            console.log("win");
+        }
     } else if (col < this.h -1 && this.board[col+1][row] === this.arrLen) {
         temp = this.board[col+1][row];
         this.board[col+1][row]= this.board[col][row];
@@ -227,7 +264,10 @@ SlidePuzzle.prototype.moveTile = function (element) {
                 'col' : (col+1),
                 'row' : row
             });
-        this.moves ++;
+        this.moveCounter();
+        if(this.checkSolve()) {
+            console.log("win");
+        }
     } else if(row < this.w -1 && this.board[col][row+1] === this.arrLen) {
         temp = this.board[col][row+1];
         this.board[col][row+1] = this.board[col][row];
@@ -247,14 +287,21 @@ SlidePuzzle.prototype.moveTile = function (element) {
                 'col' : col,
                 'row' : row
             });
-        this.moves ++;
+        this.moveCounter();
+        if(this.checkSolve()) {
+            console.log("win");
+        }
     } else if(!$($tile).hasClass("blank")){
         $tile.css('background-color', 'red');
         setTimeout(function() {
             $tile.css('background-color', 'transparent');
         },250);
     }
-    if(puzzle.checkSolve()) {
-        console.log('win');
-    }
 };
+
+SlidePuzzle.prototype.moveCounter = function() {
+    if (this.gameStart) {
+        this.moves ++;
+        $("#move-count").find("span").html(this.moves);
+    }
+}
