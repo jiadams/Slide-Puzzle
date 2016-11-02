448c06f231e23a30d8a56f6448afbe8884516ff9
Josh Adams <joshua.ian.adams@gmail.com>
2016-11-01 22:46:51 -0400

    Build out basic functionality
    Built out game funtionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
+$(document).ready(function(){
+    var puzzle = new SlidePuzzle(4,4);
+    $(document).on("click", "button#shuffle-btn", function () {
+        puzzle.shuffleBoard();
+    });
+    $(document).on("click", ".block", function () {
+        puzzle.moveTile(this);
+    });
+});
+
+
 var SlidePuzzle = function (width,height) {
     this.w = width;
     this.h = height;
+    this.arrLen = this.w * this.h;
     this.moves = 0;
-    this.board = this.buildBoard();
-    console.log(this.board);
+    this.solveState = this.buildBoard();
+    this.board = this.solveState;
+    this.drawBoard();
+    this.boardLine;
 };
 
 SlidePuzzle.prototype.buildBoard = function() {
@@ -20,9 +34,6 @@ SlidePuzzle.prototype.buildBoard = function() {
     for (i = 0; i <arrLen; i++) {
         tempArray[i] = i + 1;
     }
-    do {
-        tempArray = this.shuffle(tempArray);
-    } while (!this.solvable(tempArray));
     boardArray = this.createMDArray();
     k = 0;
     for (i = 0; i < this.h; i++) {
@@ -31,11 +42,30 @@ SlidePuzzle.prototype.buildBoard = function() {
             k++;
         }
     }
+    this.boardLine = tempArray;
     return boardArray;
 };
 
+SlidePuzzle.prototype.shuffleBoard = function() {
+    tempArray = this.boardLine;
+    do {
+        tempArray = this.shuffle(tempArray);
+    } while (!this.solvable(tempArray));
+    boardArray = this.createMDArray();
+    k = 0;
+    for (i = 0; i < this.h; i++) {
+        for (j = 0; j < this.w; j++) {
+            boardArray[i][j] = this.boardLine[k];
+            k++;
+        }
+    }
+    this.boardLine = tempArray;
+    this.board = boardArray;
+    this.drawBoard();
+};
+
 SlidePuzzle.prototype.solvable = function(boardArray) {
-    var i, j, blank, inv = 0, arrLen = boardArray.length 
+    var i, j, blank, inv = 0, arrLen = boardArray.length ;
         for(i = 0; i <arrLen; i++) {
             if (boardArray[i] < arrLen) {
                 for (j = i +1; j < arrLen; j++) {
@@ -52,7 +82,7 @@ SlidePuzzle.prototype.solvable = function(boardArray) {
             return false;
         }
     } else {
-        var blankRow = -((Math.ceil(blank / this.h )) - (this.h +1))
+        var blankRow = -((Math.ceil(blank / this.h )) - (this.h +1));
         if (!inv%2 && blankRow%2) {
             return false;
         } else if(!blankRow%2) {
@@ -88,6 +118,143 @@ SlidePuzzle.prototype.createMDArray = function() {
     return array;
 };
 
-SlidePuzzle.prototype.drawBoard {
-    
-}
+SlidePuzzle.prototype.drawBoard = function() {
+    var $gameNode = $("#game-board");
+    $gameNode.empty();
+    var i, j;
+    for(i = 0; i < this.board.length; i++) {
+        for( j = 0; j < this.board[i].length; j++) {
+            nodeValue = this.board[i][j];
+            if (nodeValue < this.arrLen) {
+                $("<div></div>").addClass("block").html(nodeValue).css({
+                    top : 100 * i,
+                    left : 100 * j
+                }).attr({
+                    'data-col' : i,
+                    'data-row' : j
+                }).appendTo($gameNode);
+            } else {
+                $("<div></div>").addClass("block blank").css({
+                    top : 100 * i,
+                    left : 100 * j
+                }).attr({
+                    'data-col' : i,
+                    'data-row' : j
+                }).appendTo($gameNode);
+            }
+            
+        }
+    }
+    $($gameNode).css({
+        width : (this.w * 100),
+        height : (this.h * 100)
+    });
+};
+
+SlidePuzzle.prototype.checkSolve = function () {
+    var i, j;
+    for(i = 0; i < this.board.length; i++) {
+        for(j = 0; j < this.board[i].length; j++) {
+            if (this.board[i][j] !== this.solveState[i][j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+};
+
+SlidePuzzle.prototype.moveTile = function (element) {
+    var temp;
+    var $tile = $(element);
+    var $blankTile = $(".block.blank");
+    var col = Number($tile.data('col')), row = Number($tile.data('row'));
+    if(col > 0 && this.board[col-1][row] === this.arrLen) {
+        temp = this.board[col-1][row];
+        this.board[col-1][row] = this.board[col][row];
+        this.board[col][row] = temp;
+        //console.log("up");
+        $tile.css({
+                top : 100 * (col-1),
+                left : 100 * row
+            }).data({
+                'col' : (col-1),
+                'row' : row
+            });
+        $blankTile.css({
+                top : 100 *  col,
+                left : 100 * row
+            }).data({
+                'col' : col,
+                'row' : row
+            });
+        this.moves ++;
+    } else if(row > 0 && this.board[col][row-1] === this.arrLen) {
+        temp = this.board[col][row-1];
+        this.board[col][row-1] = this.board[col][row];
+        this.board[col][row] = temp;
+        //console.log("left");
+        $tile.css({
+                top : 100 * col,
+                left : 100 * (row-1)
+            }).data({
+                'col' : col,
+                'row' : (row-1)
+            });
+        $blankTile.css({
+                top : 100 *  col,
+                left : 100 * row
+            }).data({
+                'col' : col,
+                'row' : row
+            });
+        this.moves ++;
+    } else if (col < this.h -1 && this.board[col+1][row] === this.arrLen) {
+        temp = this.board[col+1][row];
+        this.board[col+1][row]= this.board[col][row];
+        this.board[col][row] = temp;
+        //console.log("down");
+        $blankTile.css({
+                top : 100 *  col,
+                left : 100 * row
+            }).data({
+                'col' : col,
+                'row' : row
+            });
+        $tile.css({
+                top : 100 * (col+1),
+                left : 100 * row
+            }).data({
+                'col' : (col+1),
+                'row' : row
+            });
+        this.moves ++;
+    } else if(row < this.w -1 && this.board[col][row+1] === this.arrLen) {
+        temp = this.board[col][row+1];
+        this.board[col][row+1] = this.board[col][row];
+        this.board[col][row] = temp;
+        //console.log("right");
+        $tile.css({
+                top : 100 * col,
+                left : 100 * (row+1)
+            }).data({
+                'col' : col,
+                'row' : (row+1)
+            });
+        $blankTile.css({
+                top : 100 *  col,
+                left : 100 * row
+            }).data({
+                'col' : col,
+                'row' : row
+            });
+        this.moves ++;
+    } else if(!$($tile).hasClass("blank")){
+        $tile.css('background-color', 'red');
+        setTimeout(function() {
+            $tile.css('background-color', 'transparent');
+        },250);
+    }
+    if(puzzle.checkSolve()) {
+        console.log('win');
+    }
+};
