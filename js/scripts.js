204c3afb417967e60024174692577a4f5ce6c1fa
Josh Adams <joshua.ian.adams@gmail.com>
2016-11-07 22:17:20 -0500

    Added comments and standardized some JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,29 @@
-$(document).ready(function(){
+"use strict";
+jQuery(document).ready(function($){
+    //Handles default puzzle size as a 4x4, creates board
     var puzzle = new SlidePuzzle(4,4);
+    //Creates on click/touch even to shuffle the board when user clicks shuffle
     $(document).on("click touchstart", "button#shuffle-btn", function () {
         puzzle.shuffleBoard();
     });
+    //Calls move tile function when a tile is clicked/Touched
     $(document).on("click touchstart", ".block", function () {
         puzzle.moveTile(this);
     });
+    //Changes the size of the puzzle
     $(document).on("submit", "#puzzle-size", function (event) {
         var puzzleSize = $(this).serializeArray();
         $('#game-timer').html('Time: ');
         puzzle = new SlidePuzzle(puzzleSize[0].value, puzzleSize[1].value);
         return false;
     });
+    //Changes the background of the puzzle, limited form validation on url submission
     $(document).on("submit", "#background-form", function (event) {
         var background = $(this).serializeArray();
         $(this).find("span.warning").remove();
         $("#custom-url").removeClass('warning');
         if(background[0].value === 'custom') {
-            if (background[1].value !== "" && background[1].value.match(/\.(jpeg|jpg|gif|png)$/) != null) {
+            if (background[1].value !== "" && background[1].value.match(/\.(jpeg|jpg|gif|png)$/) !== null) {
                 puzzle.gameBackground = background[1].value;
             } else {
                 $("#custom-url").addClass('warning').before("<span class='warning'>Please Enter Valid URL for custom images</span>");
@@ -28,12 +34,23 @@ $(document).ready(function(){
         puzzle.backgroundSetter();
         return false;
     });
+    //Dismisses win animation div on click.
     $(document).on("click touchstart", ".win", function (){
-        $winModal = $(this);
-        $winModal.addClass('fade');
+        var $winModal = $(this);
+        $winModal.addClass("fade");
         setTimeout(function() {
             $winModal.remove();
         }, 1000);
     });
 });
 
+function localStorageIsSup(){
+    var test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
--- js/timer.js
+++ js/timer.js
@@ -1,24 +1,26 @@
+"use strict";
+//Timer Class used to keep track of time
 var Timer = function(element, delay) {
     var timer = createTimer(),
     offset,
     clock,
     interval;
-
+//Appends the timer to element specified in function arguments
     element.appendChild(timer);
 
     reset();
-
+//Creates element to store timer in.
     function createTimer() {
         return document.createElement("span");
     }
-
+//Start the timer
     function start() {
         if (!interval) {
           offset   = Date.now();
           interval = setInterval(update, delay);
         }
     }
-
+//Stop the timer, return timer to the Thousandth of a second
     function stop() {
         if (interval) {
             clearInterval(interval);
@@ -26,28 +28,29 @@ var Timer = function(element, delay) {
         }
         return (clock / 1000).toFixed(3);
     }
-
+//Reset timer
     function reset() {
         clock = 0;
         render();
     }
-
+//Add the Change in time since the clock was last updated to the timer
     function change() {
         var now = Date.now(),
         d = now - offset;
         offset = now;
         return d;
     }
-
+//Update the timer based on the change in time
     function update() {
         clock += change();
         render();
     }
-
+//Render the timer in the HTML to the thousandth of a second.
     function render() {
         timer.innerHTML = (clock / 1000).toFixed(3);
     }
 
+//Create API
     this.start = start;
     this.stop = stop;
     this.reset = reset;
